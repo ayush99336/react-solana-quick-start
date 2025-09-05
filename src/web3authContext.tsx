32a7710902e4cdeacbf0344029a1a21376a84a1a
ayush99336 <ayushkaushikhere@gmail.com>
2025-09-06 02:02:39 +0530

    wallet popup

--- src/web3authContext.tsx
+++ src/web3authContext.tsx
@@ -1,5 +1,5 @@
 // IMP START - Quick Start
-import { WEB3AUTH_NETWORK } from "@web3auth/modal";
+import { WEB3AUTH_NETWORK, CONFIRMATION_STRATEGY } from "@web3auth/modal";
 import { type Web3AuthContextConfig } from "@web3auth/modal/react";
 // IMP END - Quick Start
 
@@ -12,6 +12,9 @@ const web3AuthContextConfig: Web3AuthContextConfig = {
   web3AuthOptions: {
     clientId,
     web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
+    walletServicesConfig: {
+      confirmationStrategy: CONFIRMATION_STRATEGY.MODAL, // Show confirmation modal for all transactions
+    }
   }
 };
 // IMP END - Instantiate SDK
